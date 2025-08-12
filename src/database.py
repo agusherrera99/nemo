215c25c747e2ffe393530025f0703c12fbad454f
agusherrera99 <agustinherrera.dev@gmail.com>
2025-08-12 09:28:39 -0300

    add estimated deadline handling and update functionality in Database class

--- src/database.py
+++ src/database.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+from datetime import datetime, timedelta
 from pathlib import Path
 from types import TracebackType
 from typing import Optional, Union
@@ -26,7 +27,7 @@ class SafeCursor:
         self.cursor.close()
 
 
-type TaskTuple = tuple[int, str, str, str, int, int]
+type TaskTuple = tuple[int, str, str, str, str, int, int]
 type TasksTuplesList = list[TaskTuple]
 type PathOrStr = Union[str, Path]
 
@@ -50,10 +51,29 @@ class Database:
             cursor.execute(query)
             self.__connection.commit()
 
+    def _getEstimatedDeadlineDatetime(self, daysUntilDeadline: Optional[int]) -> Optional[datetime]:
+        if daysUntilDeadline:
+            today = datetime.today()
+            deadlineDatetime = today + timedelta(daysUntilDeadline)
+            return deadlineDatetime
+        return None
+
     def _pinnedFirst(self, taskTuple: TaskTuple) -> bool:
-        pinStateValue = taskTuple[4]
+        pinStateValue = int(taskTuple[5])
         isPinned = pinStateValue == TaskPinStates.PINNED.value
         return isPinned
+    
+    def getEstimatedDatetime(self, hexUUID: str) -> str:
+        query = self.__getQueryFromSQLFile("getEstimatedDeadline.sql")
+
+        params = (hexUUID,)
+        with SafeCursor(self.__connection) as cursor:
+            response = cursor.execute(query, params)
+            result = response.fetchone()
+            if result is None:
+                raise sqlite3.Error("estimated datetime not found")
+
+        return result[0]
 
     def getTasksTupleList(self) -> TasksTuplesList:
         query = self.__getQueryFromSQLFile("getTasks.sql")
@@ -70,13 +90,14 @@ class Database:
         return tasksTupleList
 
     def addTask(
-        self,
-        title: str,
-        description: str,
+        self, title: str, description: str, estimatedDeadline: Optional[int] = None
     ) -> tuple[str]:
         query = self.__getQueryFromSQLFile("addTask.sql")
         randomUUID = uuid4()
         hexadecimalString = randomUUID.hex[:8]
+        estimatedDatetime: datetime | None = self._getEstimatedDeadlineDatetime(
+            estimatedDeadline
+        )
 
         stateValue = TaskStates.INCOMPLETED.value
 
@@ -84,6 +105,7 @@ class Database:
             hexadecimalString,
             title,
             description,
+            estimatedDatetime,
             TaskPinStates.UNPINNED.value,
             stateValue,
         )
@@ -143,6 +165,33 @@ class Database:
             result = response.fetchone()
             if result is None:
                 raise sqlite3.Error("description task update should return UUID value")
+            self.__connection.commit()
+
+        success = True if result[0] else False
+        return success
+
+    def updateTaskEstimatedDeadline(self, hexUUID: str, newDaysUntilDeadline: int) -> bool:
+        query = self.__getQueryFromSQLFile("updateTaskEstimatedDeadline.sql")
+
+        actualEstimatedDatetime = datetime.strptime(
+            self.getEstimatedDatetime(hexUUID), "%Y-%m-%d %H:%M:%S.%f"
+        )
+
+        newEstimatedDeadline = None
+        if newDaysUntilDeadline < 0:
+            newEstimatedDeadline = actualEstimatedDatetime - timedelta(abs(newDaysUntilDeadline))
+        elif newDaysUntilDeadline > 0:
+            newEstimatedDeadline = actualEstimatedDatetime + timedelta(newDaysUntilDeadline)
+        else:
+            newEstimatedDeadline = actualEstimatedDatetime
+
+
+        params = (newEstimatedDeadline, hexUUID)
+        with SafeCursor(self.__connection) as cursor:
+            response = cursor.execute(query, params)
+            result = response.fetchone()
+            if result is None:
+                raise sqlite3.Error("estimatedDeadline update should return UUID value")
             self.__connection.commit()
 
         success = True if result[0] else False
